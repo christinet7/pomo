808108995f7aefed0fd1cdc72d9e925431f2e894
Christine Tsai <christinetsai7@gmail.com>
2023-05-28 17:58:43 -0400

    trying to work on script

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function start() {
-    
+const timer = {
+    pomodoro: 25,
+    shortBreak: 5,
+    longBreak: 15,
+    longBreakInterval: 4
+};
+
+let numShortBreaks = 0;
+
+document.getElementById("pomodoroButton").addEventListener("click", startPomo);
+
+function startPomo() {
+    alert("HELLO");
+
 }
