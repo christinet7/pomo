3cf73c5eff0e5c424601671c5c1c3f0fb2a2b5f9
Christine Tsai <christinetsai7@gmail.com>
2023-05-30 22:20:48 -0400

    debugged start/pause and did some styling

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const buttonList = ["pomodoroButton", "shortBreakButton", "longBreakButton", "startButton"];
 
-let numShortBreaks = 0;
+let numSessions = 0;
 let timeRemainingMins = 0;
 let timeRemainingSecs = 0;
 let totalSecs = 0;
 var interval = null;
 var countingDown = false;
+var pomodoroMode = false;
 
 for (let i = 0; i < buttonList.length; i++) {
     document.getElementById(buttonList[i]).addEventListener("click", buttonPressHandler);
@@ -30,36 +31,51 @@ function updateTimer( mode ) {
             break;
         case "startButton":
             startOrStop();
-            // pauseButtonText();
-            // pause();
-            // startCountdown();
             break;
     }
 }
 
+function changeBackgroundColor( color ) { document.body.style.background = color; }
+
 function setPomodoro() {
     // changing html of timer
-    let minutes = 25; 
+    document.getElementById("startButton").innerHTML = "start";
+    pomodoroMode = true;
+    let minutes = .2; 
     document.getElementById("timerMinutes").innerHTML = `${minutes}`;
+    document.getElementById("timerSeconds").innerHTML = "00";
     totalSecs = minutes * 60;
+    changeBackgroundColor('darkolivegreen');
 }
 
 function setShortBreak() {
-    let minutes = 1;
+    document.getElementById("startButton").innerHTML = "start";
+    pomodoroMode = false;
+    let minutes = .3;
     document.getElementById("timerMinutes").innerHTML = `${minutes}`;
+    document.getElementById("timerSeconds").innerHTML = "00";
     totalSecs = minutes * 60;
+    changeBackgroundColor("#5c310e");
 }
 
 function setLongBreak() {
+    document.getElementById("startButton").innerHTML = "start";
+    pomodoroMode = false;
     let minutes = 15;
     document.getElementById("timerMinutes").innerHTML = `${minutes}`;
+    document.getElementById("timerSeconds").innerHTML = "00";
     totalSecs = minutes * 60;
+    changeBackgroundColor("#1a5691");
 }
 
 // padstart: https://www.w3schools.com/js/tryit.asp?filename=tryjs_string_padding1
 // setInterval: https://www.w3schools.com/jsref/met_win_setinterval.asp 
 function startCountdown() {
     countingDown = true;
+    if (pomodoroMode) {
+        numSessions++;
+        document.getElementById("catPic" + String(numSessions)).style.visibility = "visible";
+    }
     if (totalSecs === 0) { return; }
     interval = setInterval( function() {
         totalSecs--;
@@ -67,41 +83,30 @@ function startCountdown() {
         let seconds = totalSecs % 60;
         document.getElementById("timerMinutes").innerHTML = String(minutes).padStart(2, "0");
         document.getElementById("timerSeconds").innerHTML = String(seconds).padStart(2, "0");
+        
+        // automatically go to next mode
         if (totalSecs === 0) {
             clearInterval(interval); 
+            if (pomodoroMode && numSessions === 4) { setLongBreak();}
+            else if (pomodoroMode) {setShortBreak();}
+            else {setPomodoro();}
         }
     }
     ,1000);
-
 }
 
 function startOrStop() {
-    // // start timer up again
-    // if (document.getElementById("startButton").innerHTML === "pause" && !countingDown) {
-    //     document.getElementById("startButton").innerHTML = "start";
-    //     startCountdown();
-    // }
     // want to pause timer 
-    if (document.getElementById("startButton").innerHTML === "pause" && countingDown) {
-        alert("stopping");
-        countingDown = false;
+    if (document.getElementById("startButton").innerHTML === "pause") {
         clearInterval(interval);
         document.getElementById("startButton").innerHTML = "start";
     }
-    
-    // starting for first time
     else {
-        alert("first time");
         document.getElementById("startButton").innerHTML = "pause"
         startCountdown();
     }
-
 }
 
-function pauseButtonText() {
-    // going from paused to unpaused 
-    document.getElementById("startButton").innerHTML = "pause";
-}
 
 
 
